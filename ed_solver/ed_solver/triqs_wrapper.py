61c25e202cff1231b2e90d0904b990fba2389321
Alexander Mazanik <alexandrmazanik@gmail.com>
2026-02-03 10:08:03 +0000

    initial_guess from baths parameters

--- ed_solver/ed_solver/triqs_wrapper.py
+++ ed_solver/ed_solver/triqs_wrapper.py
@@ -39,23 +39,32 @@ class Solver:
         self.method = method
         self.warn_seen = False
 
-        iw_mesh = MeshImFreq(beta=beta, S='Fermion', n_max=n_iw)
-        iw_arr = np.array(list(iw_mesh.values()))
-        iw2_arr = -np.array(np.abs(iw_arr) ** 2, dtype=float)
-
-        self.Sigma_iw = BlockGf(mesh=iw_mesh, gf_struct=[('up', 1), ('down', 1)])
+        self.iw_mesh = MeshImFreq(beta=beta, S='Fermion', n_max=n_iw)
+        self.Sigma_iw = BlockGf(mesh=self.iw_mesh, gf_struct=[('up', 1), ('down', 1)])
         self.Sigma_iw.zero()
         self.G_iw = self.Sigma_iw.copy()
+        self.Delta_iw = self.Sigma_iw.copy()
         
         # default initial guess for Delta
-        self.Delta_iw = self.Sigma_iw.copy()
-        for spin in ['up', 'down']:
-            self.Delta_iw[spin].data[n_iw : 2 * n_iw, 0, 0] = -4j / np.sqrt(-iw2_arr[n_iw : 2 * n_iw] + 1)
-            self.extend_to_negative_freq(self.Delta_iw[spin])
+        self.set_initial_guess()
     
     def extend_to_negative_freq(self, gf_positive):
         gf_positive.data[0 : self.n_iw] = np.conj(gf_positive.data[2 * self.n_iw : self.n_iw - 1 : -1])
-    
+
+    def set_initial_guess(self, bath=None):
+        if bath == None:
+            iw_arr = np.array(list(self.iw_mesh.values()))
+            iw2_arr = -np.array(np.abs(iw_arr) ** 2, dtype=float)
+
+            for spin in ['up', 'down']:
+                self.Delta_iw[spin].data[self.n_iw : 2 * self.n_iw, 0, 0] = -4j / np.sqrt(-iw2_arr[self.n_iw : 2 * self.n_iw] + 1)
+                self.extend_to_negative_freq(self.Delta_iw[spin])
+        else: 
+            self.Delta_iw.zero()
+            for l in range(self.nbath_max):
+                self.Delta_iw['up'] << self.Delta_iw['up'] + bath.t2_up[l] * inverse(iOmega_n - bath.eps_up[l])
+                self.Delta_iw['down'] << self.Delta_iw['down'] + bath.t2_down[l] * inverse(iOmega_n - bath.eps_down[l])
+
     def solve(self, U, nbath, h, mu, n_m=None, n_kr=None):
         
         if self.method == 'Jacobi':
