cf7b6f916f1c4cc03c115e4a851ef68f61213827
Alexander Mazanik <alexandrmazanik@gmail.com>
2026-03-11 14:08:31 +0000

    Colored bar added

---
+++ ed_solver/ed_solver/dmft/dmft_bar.py
@@ -0,0 +1,11 @@
+RED     = "\033[31m"
+GREEN   = "\033[32m"
+YELLOW  = "\033[33m"
+RESET   = "\033[0m"
+
+def bar_fmt(curr, prev):
+    if curr <= prev:
+        return f"{GREEN}{curr:.2e}{RESET}"
+    else:
+        return f"{RED}{curr:.2e}{RESET}"
+        
